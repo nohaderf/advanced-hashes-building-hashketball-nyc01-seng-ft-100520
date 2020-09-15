32a461374774c46cb6d22b3557b4c2d1cde1d4a4
Freda Hon <freda.hon@gmail.com>
2020-09-15 17:54:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,21 @@
 # Write your code here!
 def game_hash
   hashketball = {
-    
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => "Black, White",
+      :players => [{
+        
+        }
+      ]
+    },
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => "Turquoise, Purple",
+      :players => [{
+          
+        }
+      ]
+    }
   }
 end
